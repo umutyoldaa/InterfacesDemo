f6bb75db85b46ed7380c925f8f36e2f88e6797c2
Emre Koçyatağı <londonjack731@gmail.com>
2022-09-17 12:08:48 +0300

    Add project files.

---
+++ InterfacesDemo/IWorker.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal interface IWork
+    {
+        void Work();
+
+    }
+    interface IEat
+    {
+        void Eat();
+    }
+    interface IGetSalary
+    {
+        void GetSalary();
+    }
+    class Manager : IWork, IEat, IGetSalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager eats");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine(" Manager gets salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine(" Manager works");
+        }
+    }
+    class Worker : IWork,IEat,IGetSalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("worker eats");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine(" worker gets salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine(" worker works");
+        }
+    }
+    class Robot : IWork
+    {
+        public void Work()
+        {
+            Console.WriteLine("robot works");
+        }
+    }
+    class CustomerManager
+    {
+        public CustomerManager(IWork[] worker, IGetSalary[] salaries, IEat[] eats)
+        {
+
+            foreach (var func in worker)
+            {
+               
+                func.Work();
+               
+            }
+            foreach (var func in salaries)
+            {
+
+                func.GetSalary();
+
+            }
+            foreach (var func in eats)
+            {
+
+                func.Eat();
+
+            }
+        }
+    }
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterfacesDemo;
+
+Console.WriteLine("Hello, World!");
+IWork[] workers = new IWork[]
+{
+                new Worker(),
+                new Manager(),
+                new Robot()
+};
+IEat[] eaters = new IEat[]
+{
+    new Worker(),
+    new Manager()
+};
+IGetSalary[] salaries = new IGetSalary[]
+{
+    new Worker(),
+    new Manager()
+
+};
+CustomerManager customer = new CustomerManager(workers,salaries,eaters);
